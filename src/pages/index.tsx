07bea7422f547a6e86481882a27d5cd9503b380f
gustavogt <gustavogt.app@gmail.com>
2024-06-17 22:02:30 -0300

    fix responsive cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,13 @@ import Speakers from "@/components/Speakers";
 
 export default function Home() {
   return (
-    <main className="container flex p-4 flex-col mx-auto w-full text-white/90">
+    <main className="container flex flex-col w-full text-white/90">
       <Explorer />
+<<<<<<< HEAD
       <div className="container mx-auto mt-32 gap-16 flex flex-col px-4">
+=======
+      <div className="container md:px-16 px-4 w-full mt-32 gap-16 flex flex-col items-center">
+>>>>>>> 60f96aa (fix responsive cards)
         <About />
         <Location />
         <Sponsors />
