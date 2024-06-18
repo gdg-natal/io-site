9154b6c5959b4ac2d0eb70e984add4549c02fac2
gustavogt <gustavogt.app@gmail.com>
2024-06-17 22:02:30 -0300

    fix responsive cards

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,7 @@ interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 export default function Button({ outlined, href, target, children }: IButtonProps) {
-  const styles = `px-6 py-4 border-2 rounded-full ${outlined ? 'border-lightGrey bg-transparent' : 'border-transparent bg-royalBlue'}`
+  const styles = `px-6 py-2 md:py-4 w-max border-2 flex justify-center items-center rounded-full ${outlined ? 'border-lightGrey bg-transparent' : 'border-transparent bg-royalBlue'}`
 
   return (
     <a href={href ?? '#'} target={target} className={styles}>
--- src/components/Explore/index.tsx
+++ src/components/Explore/index.tsx
@@ -14,13 +14,13 @@ export default function Explorer() {
 
   return (
     <section id="explorer" className="w-full md:pb-40 bg-[url('/images/explore-bg.png')] bg-bottom bg-no-repeat bg-contain">
-      <div className="container flex flex-col md:flex md:flex-row md:justify-center mx-auto py-16 pb-40">
+      <div className="container flex flex-col md:flex md:flex-row md:justify-center md:items-center mx-auto md:px-16 px-4 py-16 pb-40">
         <aside className='w-full flex justify-center pt-8 md:py-0 order-2 md:order-1'>
           {!loading && <Countdown eventTime={content?.eventTime} />}
         </aside>
         <aside className="md:px-16 flex flex-col items-start w-full order-1 md:order-2">
           <h1 className="mb-4 text-4xl">{content?.title}</h1>
-          <h2 className="mb-8 text-xl">{content?.description}</h2>
+          <h2 className="mb-8 text-lg">{content?.description}</h2>
           <Button href={content?.registrationUrl} target="_blank">Registre-se</Button>
         </aside>
       </div>
