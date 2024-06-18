9b20788d688632b21c40fb9f703c9b1d4bf4b852
Izanderson Florencio <izandersonf@gmail.com>
2024-06-17 21:50:33 -0300

    feat: improve speakers navigation

--- src/components/Speakers/SpeakerCard.tsx
+++ src/components/Speakers/SpeakerCard.tsx
@@ -7,7 +7,7 @@ interface ISpeakerCardProps {
 
 export default function SpeakerCard({ name, description, title, image }: ISpeakerCardProps) {
   return (
-    <li className="bg-lightGrey rounded-lg border-2 border-solid border-lightGrey">
+    <li className="bg-lightGrey rounded-lg border-2 border-solid border-lightGrey min-w-[300px] md:w-1/4 snap-start">
       <div id="profile-photo" className="bg-black rounded-lg py-8 bg-speakersBg bg-center bg-[length:95%] bg-no-repeat bg-[center_bottom]">
         <figure className="w-1/2 mx-auto rounded-full overflow-hidden">
           <img src={image} alt={`${name} photo`} width="100%" height="100%" />
--- src/components/Speakers/index.tsx
+++ src/components/Speakers/index.tsx
@@ -17,7 +17,7 @@ export default function Speakers() {
   return (
     <section id="speakers">
       <h2 className="mb-8 text-4xl">Palestrantes</h2>
-      <ul className="grid sm:grid-cols-2 md:grid-cols-4 gap-8">
+      <ul className="flex gap-6 overflow-x-auto snap-mandatory snap-x">
         {content?.map(((item: any) => <SpeakerCard key={item.name} {...item} />))}
       </ul>
     </section>
