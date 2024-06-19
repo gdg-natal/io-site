6c07fb0a966f9f05230544da2dd327c41e298c51
Izanderson Florencio <izandersonf@gmail.com>
2024-06-19 16:32:00 -0300

    fix: Explore responsivity

--- src/components/Explore/index.tsx
+++ src/components/Explore/index.tsx
@@ -13,12 +13,12 @@ export default function Explorer() {
   const content = data?.explore?.data?.attributes
 
   return (
-    <section id="explorer" className="w-full md:pb-40 bg-[url('/images/explore-bg.png')] bg-bottom bg-no-repeat bg-contain">
-      <div className="container flex flex-col md:flex md:flex-row md:justify-center md:items-center mx-auto px-4 py-16 pb-40">
-        <aside className='w-full flex justify-center pt-8 md:py-0 order-2 md:order-1'>
+    <section id="explorer" className="w-full md:pb-40 sm:bg-[url('/images/explore-bg.png')] bg-no-repeat md:bg-contain bg-[bottom_right]">
+      <div className="container flex flex-col sm:items-center sm:flex-row mx-auto px-4 py-8 sm:pt-16 sm:pb-40">
+        <aside className='sm:w-1/2 flex justify-center pt-8 md:py-0 order-2 md:order-1'>
           {!loading && <Countdown eventTime={content?.eventTime} />}
         </aside>
-        <aside className="md:px-16 flex flex-col items-start w-full order-1 md:order-2">
+        <aside className="sm:w-1/2 sm:pl-16 xl:pr-48 flex flex-col items-start order-1 md:order-2">
           <h1 className="mb-4 text-4xl">{content?.title}</h1>
           <h2 className="mb-8 text-lg">{content?.description}</h2>
           <Button href={content?.registrationUrl} target="_blank">Registre-se</Button>
